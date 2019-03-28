6e49ec1ee3789a24108ec8038bbb26e760453c34
Greg Toombs <gtoombs@lakeheadu.ca>
2019-03-27 22:32:12 -0400

    Initial drops

--- preprocess.py
+++ preprocess.py
@@ -6,23 +6,34 @@ import json
 class Item:
     def __init__(self, data):
         self.data = data
+        self.__dict__.update(data)
 
     @property
     def keep(self):
         return (
-            (not self.data['archived']) and
-            (self.data.get('cost') or self.data.get('recipe'))
+            (not self.archived) and
+            (
+                any(self.data.get(k) for k in ('cost', 'recipe', 'recipes'))
+                or 'mining-hardness' in self.data
+                or self.title in {'Crude oil',
+                                  'Water',
+                                  'Space science pack',
+                                  'Steam'}
+            )
         )
+
+
+def trim(items):
+    to_delete = tuple(k for k, v in items.items() if not v.keep)
+    print(f'Dropping {len(to_delete)} items...')
+    for k in to_delete:
+        del items[k]
 
 
 def main():
     with open('recipes.json') as f:
-        items = [Item(d) for d in json.load(f)]
-
-    print('Not interesting:')
-    print('\n'.join(i.data['title']
-                    for i in items
-                    if not i.keep))
+        items = {k: Item(d) for k, d in json.load(f).items()}
+    trim(items)
 
 
 if __name__ == '__main__':
