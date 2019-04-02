0bb544f54b759380c34d80fc85cd45038a94bd7e
Greg Toombs <gtoombs@lakeheadu.ca>
2019-04-02 14:51:10 -0400

    Lots more edge cases, still missing water

--- preprocess.py
+++ preprocess.py
@@ -4,90 +4,16 @@ import json, re
 from typing import Dict, Iterable
 
 
-power_re = re.compile(r'([0-9.]+) ([kMG])W electric$')
+power_re = re.compile(r'([0-9.]+) .*([kMG])(W|J)')
 
 si_facs = {
     c: 10**(3*i) for i, c in enumerate(('', 'k', 'M', 'G'))
 }
 
 
-def parse_power(s):
+def parse_power(s: str) -> float:
     m = power_re.match(s)
     return float(m[1]) * si_facs[m[2]]
-
-
-class Recipe:
-    def __init__(self, rates: dict = None, title: str = ''):
-        if rates is None:
-            self.rates = {}
-        else:
-            self.rates = rates
-        self.title = title
-
-    @staticmethod
-    def parse_side(s: str) -> Dict[str, float]:
-        out = {}
-        for pair in s.split('+'):
-            k, v = pair.split(',')
-            out[k.strip()] = float(v.strip())
-        return out
-
-    @classmethod
-    def parse_recipe(cls, s, output_title) -> Dict[str, float]:
-        if '=' in s:
-            inputs, outputs = s.split('=')
-            recipe = cls.parse_side(outputs)
-        else:
-            inputs = s
-            recipe = {output_title: 1}
-
-        inputs = cls.parse_side(inputs)
-        t = inputs.pop('Time')
-        for k in recipe:
-            recipe[k] /= t
-        for k, v in inputs.items():
-            recipe[k] = -v/t
-
-        return recipe
-
-    @classmethod
-    def from_str(cls, s: str, output_title: str):
-        r = Recipe()
-        r.rates = cls.parse_recipe(s, output_title)
-        return r
-
-    def mutate_for_producer(self, producer):
-        self.multiply_producer(producer)
-
-        self.rates.update({
-            'Pollution': float(producer.pollution),
-            'Energy': -parse_power(producer.energy)
-        })
-
-    def multiply_producer(self, prod):
-        rate = float(prod.crafting_speed)
-        for k in self.rates:
-            self.rates[k] *= rate
-
-    def dupe_for_producer(self, producer, resource: str):
-        title = f'{resource} ({producer.title})'
-        rec = Recipe(dict(self.rates), title)
-        rec.mutate_for_producer(producer)
-        return rec
-
-
-class MiningRecipe(Recipe):
-    def __init__(self, title: str, mining_hardness: float, mining_time: float):
-        super().__init__(title=title)
-        self.mining_hardness, self.mining_time = mining_hardness, mining_time
-
-    def multiply_producer(self, prod):
-        power = float(prod.mining_power)
-        speed = float(prod.mining_speed)
-        self.rates = {
-            self.title: (power - self.mining_hardness) *
-            speed / self.mining_time
-        }
 
 
 class Item:
@@ -95,10 +21,16 @@ class Item:
         self.data = data
         self.title, self.archived, self.producers, self.prototype_type, \
             self.crafting_speed, self.pollution, self.energy, self.recipe, \
-            self.mining_hardness, self.mining_time \
-            = (None,)*10
+            self.mining_hardness, self.mining_time, self.cost, \
+            self.cost_multiplier, self.recipes, self.valid_fuel, \
+            self.fuel_value, self.mining_power, self.mining_speed = (None,)*17
         self.__dict__.update({k.replace('-', '_'): v
                               for k, v in data.items()})
+        if self.prototype_type == 'technology':
+            self.producers = 'Lab'
+
+    def __str__(self) -> str:
+        return self.title
 
     @property
     def keep(self) -> bool:
@@ -114,29 +46,214 @@ class Item:
             )
         )
 
-    def for_producers(self, base_rec: Recipe) -> Iterable[Recipe]:
-        return (base_rec.dupe_for_producer(p, self.title)
-                for p in parse_producers(self.producers))
-
-    def get_recipes(self) -> Iterable[Recipe]:
-        if 'recipe' in self.data:
-            base_rec = Recipe.from_str(self.recipe, self.title)
-        elif 'mining-hardness' in self.data:
-            base_rec = MiningRecipe(self.title,
-                                    float(self.mining_hardness),
-                                    float(self.mining_time))
+    def get_recipes(self) -> Iterable:
+        if self.recipes:
+            for rates in self.recipes:
+                fac = RecipeFactory(self, rates=rates)
+                yield from fac.make()
         else:
-            raise NotImplementedError()
-        yield from self.for_producers(base_rec)
+            fac = RecipeFactory(self)
+            yield from fac.make()
+
+    def mine_rate(self, mining_hardness: float, mining_time: float) -> float:
+        return (
+                (float(self.mining_power) - mining_hardness)
+                * float(self.mining_speed) / mining_time
+        )
 
 
 all_items: Dict[str, Item] = None
+
+
+class ManualMiner:
+    def __init__(self, tool: Item):
+        self.tool = tool
+        self.title = f'Manual with {tool}'
+
+    def __str__(self) -> str:
+        return self.title
+
+    def mine_rate(self, mining_hardness: float, mining_time: float) -> float:
+        return (
+                0.6 * (float(self.tool.mining_power) - mining_hardness)
+                / mining_time
+        )
+
+
+class Recipe:
+    def __init__(self, resource: str, producer: Item, rates: dict,
+                 title: str = None):
+        self.resource = resource
+        if title:
+            self.title = title
+        else:
+            self.title = f'{resource} ({producer})'
+
+        self.rates = rates
+        self.producer = producer
+        self.multiply_producer(producer)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def multiply_producer(self, prod: Item):
+        rate = float(prod.crafting_speed)
+        for k in self.rates:
+            self.rates[k] *= rate
+
+
+class MiningRecipe(Recipe):
+    def __init__(self, resource: str, producer: Item, rates: dict,
+                 mining_hardness: float, mining_time: float, title: str = ''):
+        self.mining_hardness, self.mining_time = mining_hardness, mining_time
+        super().__init__(resource, producer, rates, title)
+
+    def multiply_producer(self, miner: Item):
+        self.rates[self.resource] = self.producer.mine_rate(
+            self.mining_hardness, self.mining_time
+        )
+
+
+class TechRecipe(Recipe):
+    def __init__(self, resource: str, producer: Item, rates: dict,
+                 cost_multiplier: float):
+        self.cost_multiplier = cost_multiplier
+        super().__init__(resource, producer, rates)
+
+    def multiply_producer(self, lab: Item):
+        self.rates[self.resource] /= self.cost_multiplier
+
+
+class PumpjackRecipe(Recipe):
+    def multiply_producer(self, pumpjack: Item):
+        # Assume default 100% yield
+        self.rates[self.resource] = 10
+
+
+class RecipeFactory:
+    def __init__(self, resource: Item, rates: dict = None):
+        self.resource = resource
+        self.producers = ()
+        if rates:
+            self.producers = (all_items[rates['building']],)
+            self.title = rates['process']
+            self.rates = self.calc_recipe(rates['inputs'], rates['outputs'])
+        else:
+            self.title = None
+            recipe = resource.recipe or resource.cost
+            if recipe:
+                self.rates = self.parse_recipe(recipe)
+                if resource.prototype_type == 'technology':
+                    self.producers = (all_items['Lab'],)
+            else:
+                if resource.mining_time or resource.title == 'Crude oil':
+                    self.rates = {}
+                else:
+                    raise NotImplementedError()
+            if (not self.producers) and (resource.mining_time or recipe) and \
+               resource.title != 'Raw wood':
+                self.producers = tuple(parse_producers(resource.producers))
+
+    @staticmethod
+    def parse_side(s: str) -> Dict[str, float]:
+        out = {}
+        for pair in s.split('+'):
+            k, v = pair.split(',')
+            out[k.strip()] = float(v.strip())
+        return out
+
+    @staticmethod
+    def calc_recipe(inputs: Dict[str, float],
+                    outputs: Dict[str, float]) -> Dict[str, float]:
+        rates = dict(outputs)
+        t = inputs.pop('Time')
+        for k in rates:
+            rates[k] /= t
+        for k, v in inputs.items():
+            rates[k] = -v / t
+        return rates
+
+    def parse_recipe(self, recipe: str) -> Dict[str, float]:
+        if '=' in recipe:
+            inputs, outputs = recipe.split('=')
+            outputs = self.parse_side(outputs)
+        else:
+            inputs = recipe
+            outputs = {self.resource.title: 1}
+
+        return self.calc_recipe(self.parse_side(inputs), outputs)
+
+    def for_energy(self, cls, **kwargs) -> Iterable[Recipe]:
+        for producer in self.producers:
+            rates = dict(self.rates)
+
+            if producer.pollution:
+                rates['Pollution'] = float(producer.pollution)
+
+            energy = -parse_power(producer.energy)
+            if 'electric' in producer.energy:
+                rates['Energy'] = energy
+                yield cls(self.resource.title, producer, rates, **kwargs)
+            elif 'burner' in producer.energy:
+                for fuel_name in producer.valid_fuel.split('+'):
+                    fuel_name = fuel_name.strip()
+                    fuel = all_items[fuel_name]
+                    fuel_value = parse_power(fuel.fuel_value)
+                    rates_with_fuel = dict(rates)
+                    rates_with_fuel[fuel.title] = energy / fuel_value
+                    kwargs['title'] = (f'{self.resource} '
+                                       f'({producer} '
+                                       f'fueled by {fuel_name})')
+                    yield cls(self.resource.title, producer,
+                              rates_with_fuel, **kwargs)
+            else:
+                raise NotImplementedError()
+
+    tree_re = re.compile(r'(\d+) .*?\|([^}|]+)\}')
+
+    def wood_mining(self) -> Iterable[MiningRecipe]:
+        miners = tuple(
+            ManualMiner(tool)
+            for tool in all_items.values()
+            if tool.prototype_type == 'mining-tool'
+        )
+        for m in self.tree_re.finditer(self.resource.mining_time):
+            mining_time, source = int(m[1]), m[2]
+            for miner in miners:
+                yield MiningRecipe(self.resource.title, miner, {},
+                                   float(self.resource.mining_hardness),
+                                   mining_time,
+                                   title=f'{self.resource} '
+                                   f'({miner} from {source})')
+
+    def make(self) -> Iterable[Recipe]:
+        if self.rates:
+            if self.resource.prototype_type == 'technology':
+                yield TechRecipe(self.resource.title, self.producers[0],
+                                 self.rates,
+                                 float(self.resource.cost_multiplier))
+            else:
+                yield from self.for_energy(Recipe, title=self.title)
+        elif self.resource.title == 'Raw wood':
+            yield from self.wood_mining()
+        elif self.resource.mining_time:
+            yield from self.for_energy(
+                MiningRecipe,
+                mining_hardness=float(self.resource.mining_hardness),
+                mining_time=float(self.resource.mining_time))
+        elif self.resource.title == 'Crude oil':
+            yield from self.for_energy(PumpjackRecipe)
+        else:
+            raise NotImplementedError()
 
 
 def parse_producers(s: str) -> Iterable[Item]:
     if s == 'Furnace':
         return (i for i in all_items.values()
                 if i.prototype_type == 'furnace')
+    if s == 'Assembling machine':
+        return (i for i in all_items.values()
+                if i.prototype_type == 'assembling-machine')
     return (all_items[p.strip()] for p in s.split('+')
             if 'manual' not in p.lower())
 
@@ -179,10 +296,14 @@ def main():
     recipes = []
     resources = set()
     for item in all_items.values():
-        item_recipes = item.get_recipes()
+        try:
+            item_recipes = tuple(item.get_recipes())
+        except Exception as e:
+            print(e)
+            continue
         recipes.extend(item_recipes)
         for recipe in item_recipes:
-            resources.update(recipe.keys())
+            resources.update(recipe.rates.keys())
 
 
 if __name__ == '__main__':
