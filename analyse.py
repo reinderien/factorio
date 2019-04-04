453642410af1a91a8c18ae26681deb57a22f1a75
greg_toombs <greg.toombs@oracle.com>
2019-04-04 13:44:47 -0400

    Things are loaded

--- analyse.py
+++ analyse.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import json, lzma
+from scipy.sparse import load_npz
+from typing import Sequence
+
+
+def load_meta(fn) -> (Sequence[str], Sequence[str]):
+    print(f'Loading {fn}...')
+    with lzma.open(fn) as f:
+        meta = json.load(f)
+    return meta['recipes'], meta['resources']
+
 
 def main():
-    pass
+    fn = 'recipes.npz'
+    print(f'Loading {fn}...')
+    recipes = load_npz(fn)
+
+    recipe_names, resource_names = load_meta('recipes-meta.json.xz')
 
 
 if __name__ == '__main__':
