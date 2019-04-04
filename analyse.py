8c4b91b2d799bbb3afe317a49243f95d68515159
greg_toombs <greg.toombs@oracle.com>
2019-04-04 15:26:48 -0400

    More loading info

--- analyse.py
+++ analyse.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python3
 
 import json, lzma
-from scipy.sparse import load_npz
+from scipy.sparse import csr_matrix, load_npz
 from typing import Sequence
 
 
 def load_meta(fn) -> (Sequence[str], Sequence[str]):
-    print(f'Loading {fn}...')
+    print(f'Loading {fn}... ', end='')
     with lzma.open(fn) as f:
-        meta = json.load(f)
-    return meta['recipes'], meta['resources']
+        meta: dict = json.load(f)
+    recipes, resources = meta['recipes'], meta['resources']
+    print(f'{len(recipes)} recipes, {len(resources)} resources')
+    return recipes, resources
+
+
+def load_matrix(fn) -> csr_matrix:
+    print(f'Loading {fn}... ', end='')
+    recipes: csr_matrix = load_npz(fn)
+    print(f'{recipes.shape[0]}x{recipes.shape[1]}, {recipes.nnz} nnz, '
+          f'{recipes.nnz / recipes.shape[0] / recipes.shape[1]:.1%} density')
+    return recipes
 
 
 def main():
-    fn = 'recipes.npz'
-    print(f'Loading {fn}...')
-    recipes = load_npz(fn)
-
+    recipes = load_matrix('recipes.npz')
     recipe_names, resource_names = load_meta('recipes-meta.json.xz')
 
 
